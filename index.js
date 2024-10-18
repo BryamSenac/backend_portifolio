37d74b9aade6a47b62bb609b0789949872c54f29
BryamSenac <bryam.assolini@docente.pr.senac.br>
2024-10-18 16:17:46 -0300

    att visuais dos metodos GET, POST, PUT e DELETE

--- index.js
+++ index.js
@@ -9,53 +9,53 @@ app.use(express.json());
 
 let cartoes = [
     {
-        nome: 'Tenis Barcelona',
-        valor: 'R$59.70',
-        img: "https://th.bing.com/th/id/R.720c0e20b9a11f1849c7c7c87b373437?rik=gcjvH6x1SapnLw&pid=ImgRaw&r=0"
+        linguagem: 'WEB - API Maps',
+        nome: 'Transporte Publico',
+        img: "https://th.bing.com/th/id/R.946a290b98381829deefd64faef6c605?rik=djUs5Mq0z3ZRiQ&pid=ImgRaw&r=0"
     },
     {
-        nome: 'CARTAO 2',
-        valor: 'R$90.23',
+        linguagem: 'CARTAO 2',
+        nome: 'R$90.23',
         img: "https://th.bing.com/th/id/OIP.zZcHBkUfwUmtek-sWpnjaAHaHa?w=1200&h=1200&rs=1&pid=ImgDetMain"
     },
     {
-        nome: 'CARTAO 3',
-        valor: 'R$27.79',
+        linguagem: 'CARTAO 3',
+        nome: 'R$27.79',
         img: "https://th.bing.com/th/id/OIP.9kvRZ7zLIEE2umY0x-g9QwHaHa?w=544&h=544&rs=1&pid=ImgDetMain"
     },
     {
-        nome: 'CARTAO 4',
-        valor: 'R$86.49',
+        linguagem: 'CARTAO 4',
+        nome: 'R$86.49',
         img: "https://cdnv2.moovin.com.br/sandycalcados/imagens/produtos/original/tenis-nike-flyknit-max-85e744676043564192c692461cdb747f.jpg"
     },
     {
-        nome: 'CARTAO 5',
-        valor: 'R$11.55',
+        linguagem: 'CARTAO 5',
+        nome: 'R$11.55',
         img: "https://th.bing.com/th/id/OIP.Oe1oRos3_P7NZW5k8A6VqAAAAA?w=474&h=474&rs=1&pid=ImgDetMain"
     },
     {
-        nome: 'CARTAO 6',
-        valor: 'R$33.77',
+        linguagem: 'CARTAO 6',
+        nome: 'R$33.77',
         img: "https://th.bing.com/th/id/OIP.X7wo2DnEcRJdI9oMPh2RMQHaHa?w=650&h=650&rs=1&pid=ImgDetMain0"
     },
     {
-        nome: 'CARTAO 7',
-        valor: 'R$12.36',
+        linguagem: 'CARTAO 7',
+        nome: 'R$12.36',
         img: "https://th.bing.com/th/id/OIP.oru-V2rGQt6PTXXuh2qFDgHaIq?w=850&h=995&rs=1&pid=ImgDetMain"
     },
     {
-        nome: 'CARTAO 8',
-        valor: 'R$38.80',
+        linguagem: 'CARTAO 8',
+        nome: 'R$38.80',
         img: "https://th.bing.com/th/id/OIP.mODcPB1d4wGyUkxwD63-yQHaHa?w=2000&h=2000&rs=1&pid=ImgDetMain"
     },
     {
-        nome: 'CARTAO 9',
-        valor: 'R$14.87',
+        linguagem: 'CARTAO 9',
+        nome: 'R$14.87',
         img: "https://th.bing.com/th/id/OIP.O5S1ytIVLkSa8ZuqsNOongHaHa?rs=1&pid=ImgDetMain"
     },
     {
-        nome: 'CARTAO 10',
-        valor: 'R$53.40',
+        linguagem: 'CARTAO 10',
+        nome: 'R$53.40',
         img: "https://th.bing.com/th/id/OIP.O5S1ytIVLkSa8ZuqsNOongHaHa?rs=1&pid=ImgDetMain"
     }
 ];
@@ -67,22 +67,22 @@ app.get('/cartoes', (req, res) => {
 });
 
 app.post('/cartoes', (req, res) => {
-    const { nome, valor, img } = req.body;
+    const { linguagem, nome, img } = req.body;
 
-    if (nome == null || nome === '') {
-        res.status(400).json({ mensagem: 'Nome do cartão invalido!' });
-        console.log('Novo cartão não cadastrado, nome invalido!');
+    if (linguagem == null || linguagem === '') {
+        res.status(400).json({ mensagem: 'linguagem do cartão invalido!' });
+        console.log('Novo cartão não cadastrado, linguagem invalido!');
 
-    } else if (valor == null || valor === '' || isNaN(valor)) {
-        res.status(400).json({ mensagem: 'Valor do cartão invalido!' });
-        console.log('Novo cartão não cadastrado, valor invalidao');
+    } else if (nome == null || nome === '') {
+        res.status(400).json({ mensagem: 'nome do cartão invalido!' });
+        console.log('Novo cartão não cadastrado, nome invalidao');
 
     } else if (img == null || img === '') {
         res.status(400).json({ mensagem: 'Imagem do cartão invalido!' });
         console.log('Novo cartão não cadastrado, imagem invalida');
 
     } else {
-        cartoes.push({ nome: nome, valor: valor, img: img });
+        cartoes.push({ linguagem: linguagem, nome: nome, img: img });
         res.status(201).json({ mensagem: 'Cartão cadastrado com sucesso!!' });
         console.log('Novo cartão cadastrado!');
     }
@@ -103,22 +103,21 @@ app.delete('/cartoes', (req, res) => {
 });
 
 app.put('/cartoes', (req, res) => {
-    const { nome, valor, img, id } = req.body;
+    let { linguagem, nome, img, id } = req.body;
     if (!id || id < 0 || id > cartoes.length) {
         res.status(400).json({ mensagem: 'Cartão ' + id + ' não encontrado' });
         console.log('Cartão não atualizado, id invalido.');
-
     } else {
+        if (linguagem === null || linguagem === '') {
+            linguagem = cartoes[id].linguagem;
+        } 
         if (nome == null || nome === '') {
             nome = cartoes[id].nome;
-        } 
-        if (valor == null || valor === '' || isNaN(valor)) {
-            valor = cartoes[id].valor;
         } 
         if (img == null || img === '') {
             img = cartoes[id].img;
         }
-        cartoes[id] = { nome: nome, valor: valor, img: img };
+        cartoes[id] = { linguagem: linguagem, nome: nome, img: img };
         res.status(201).json({ mensagem: 'Cartão ' + id + ' atualizado' });
         console.log('Cartão ' + id + ' atualizado');
     }
